f64ba26279cfaa85c9385a50327232a2c5ab5354
Yakamozm <joshuafollente@gmail.com>
2026-03-11 14:27:10 -0700

    Calculations for expenses added
    Added JavaScript that allows calculation for all the expenses

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const expenseInput = document.getElementById("exp1");
+const needsExpensesInputs = Array.from(document.querySelectorAll('.needs-tab .expenses-section input'));
+const wantsExpensesInputs = Array.from(document.querySelectorAll('.wants-tab .expenses-section input'));
+const savingsExpensesInputs = Array.from(document.querySelectorAll('.savings-tab .expenses-section input'));
+const allExpenseInputs = needsExpensesInputs.concat(wantsExpensesInputs.concat(savingsExpensesInputs));
+// add consts for updating screen, all the expense display boxes
+const needsExpensesDisplay = document.querySelector('.needs-tab .expense-total div');
+const wantsExpensesDisplay = document.querySelector('.wants-tab .expense-total div');
+const savingsExpensesDisplay = document.querySelector('.savings-tab .expense-total div');
+const needsExpensesDisplay2 = document.querySelector('#needs-total');
+const wantsExpensesDisplay2 = document.querySelector('#wants-total');
+const savingsExpensesDisplay2 = document.querySelector('#savings-total');
+const totalExpenseDisplay = document.querySelector('#total');
 
+console.log(needsExpensesDisplay);
+console.log(wantsExpensesDisplay);
+console.log(savingsExpensesDisplay);
 
 
 // Add listener to find the value income_input
@@ -8,14 +22,14 @@ let needsExpenses = 0;
 let wantsExpenses = 0;
 let savingsExpenses = 0;
 let totalExpenses = 0;
+let income = 0;
 
-const input_value = expenseInput;
-
-const need_target = input_value / 2;
-const wants_target = input_value * 0.3;
-const savings_target = input_value * 0.2;
+const need_target = income * 0.5;
+const wants_target = income * 0.3;
+const savings_target = income * 0.2;
 
 
+//functions
 function calculateFederalTax(taxableIncome) { 
     let tax = 0;
 
@@ -34,3 +48,45 @@ function calculateFederalTax(taxableIncome) {
 
     return tax;
 }
+
+function setInputEventListeners() {
+    for (input of allExpenseInputs) {
+        input.addEventListener('change', () => {
+            calculateAllExpenses();
+            updateScreen();
+        })
+    }
+}
+
+function calculateAllExpenses() {
+    needsExpenses = 0;
+    wantsExpenses = 0;
+    savingsExpenses = 0;
+    totalExpenses = 0;
+    for (input of needsExpensesInputs) {
+        needsExpenses = needsExpenses + Number(input.value);
+    }
+    for (input of wantsExpensesInputs) {
+        wantsExpenses = wantsExpenses + Number(input.value);
+    }
+    for (input of savingsExpensesInputs) {
+        savingsExpenses = savingsExpenses + Number(input.value);
+    }
+    totalExpenses = needsExpenses + wantsExpenses + savingsExpenses;
+}
+
+function updateScreen() {
+    needsExpensesDisplay.innerHTML = needsExpenses;
+    wantsExpensesDisplay.innerHTML = wantsExpenses;
+    savingsExpensesDisplay.innerHTML = savingsExpenses;
+    needsExpensesDisplay2.value = needsExpenses;
+    wantsExpensesDisplay2.value = wantsExpenses;
+    savingsExpensesDisplay2.value = savingsExpenses;
+    totalExpenseDisplay.value = totalExpenses;
+}
+
+//Event Listeners
+setInputEventListeners();
+
+//Initialization
+updateScreen();
